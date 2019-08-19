c2bd343e5267d5b9e4d12dca877a3213df4cf468
Reis472 <bwong28@binghamton.edu>
2019-08-18 22:46:35 -0400

    Final touches being done

--- lfs.cpp
+++ lfs.cpp
@@ -24,23 +24,16 @@ struct inode{
 	int dblock[128];
 	char dummy[384];
 };
-//struct for inum so it can point to an inode
-/*struct inum{
-	int num;
-	inode* nodeptr;
-	inum(){
-		this -> num = -1;
-		this -> nodeptr = nullptr;
-	}
-};*/
 
 unsigned int imap[IMAP_BLOCKS*BLOCK_SIZE];
-//inum* inumList[40];
+unsigned int checkpoint[40];
 int current_segment = 0;
 int current_block = 0;
 int current_position = 0;
 char segment[SEGMENT_SIZE];
 char segment_summary[SEGMENT_BLOCKS][2];
+char segment_status[64] = {0};
+
 //initializing new list of inum
 void initInumList(){
 	for(int i = 0; i < 40; i++){
@@ -61,7 +54,7 @@ void import(string filename, string lfs){
 	if(imap.size() == IMAP_BLOCKS){
 		cout << "The imap is currently full" << endl;
 	}
-	//check that lfs name isn't larger than 128 bytes
+	//check that lfs name isn't larger than 255 bytes
 	else if(lfs.length() > 255){
 		cout << "The name of the file should not exceed 255 bytes" << endl;
 	}
@@ -118,36 +111,6 @@ void import(string filename, string lfs){
 		current_position = (size*1024)+ charCount;
 		
 	}
-	/*else{
-		inode* node = new inode();
-		node -> name = lfs;
-		ifstream file(filename, ios::binary | ios::ate);
-		int fileLength = file.tellg();
-		int charCount = 0;
-		char buffer[BLOCK_SIZE] = {0};
-		file.open(filename);
-		for(int i = 0; i < fileLength; i++){
-			if(charCount == BLOCK_SIZE || i == fileLength-1){
-				//save and empty out buffer contents
-				memset(buffer, 0, sizeof(buffer));
-				charCount = 0;
-			}
-			file.get(buffer[i]);
-			charCount++;
-		}
-		//looking for next available inode, one with a num of -1
-		for(int i = 0; i < 40; i++){
-			if(inumList[i] -> num == -1){
-				inumList[i] -> num = i;
-				inumList[i] -> nodeptr = node;
-				filemap[i] = filename;
-				break;
-			}
-		}
-
-		node -> size = fileLength;
-		imap.push_back(node); //adding inode to vector of inodes in use
-	}*/
 }
 
 void remove(string lfs){
@@ -249,7 +212,93 @@ void display(string lfs, string number, string startbyte){
 	cout << "No block position was found" << endl;
 }	
 
+void list(){
+	//may need to add a way to get file size
+	cout << "Current Segment: " << current_segment << endl;
+	ifstream in("DRIVE/FILEMAP");
+	for(int i = 0; i < 10240; i++){
+		in.seekg(i*256);
+		char status[1];
+		in.read(status, 1);
+		if(status[0] == 1){
+			char name[255];
+			in.read(name, 255);
+			cout << name << endl;
+		}
+	}
+	in.close();
+}
+
+/*void clean(string value){
+	int val = stoi(value);
+	char target_segments[val];
+	int num = 0;
+
+	for(int i = 0; i < 64 && num < val; i++){
+*/
+
+void exit(){
+	//writing out checkpoint
+	fstream check("DRIVE/CHECKPOINT", ios::binary | ios::ate);
+	char temp[160];
+	memcpy(temp, checkpoint, 160);
+	check.write(temp, 160);
+	check.write(segment_status, current_segment); //remembers status of all segments and current one in use for next time
+	check.close();
+	//writing out current segment
+	fstream seg("DRIVE/SEGMENT"+to_string(current_segment), ios::binary | ios::ate);
+	seg.write(segment, 1024*1024);
+	seg.write(reinterpret_cast<const char*>(&segment_summary), 1024*1024);
+	seg.close();
+}
+
 int main(int argc, char* argv[]){
+	bool state = true;
+	string response;
+	string temp1;
+	string temp2;
+	string temp3;
+	//add way to find the next clean segment
+	while(state){
+		cout << "Pick a following command: IMPORT, REMOVE, DISPLAY, LIST, CLEAN, EXIT" << endl;
+		cin >> response;
+		if(response == "IMPORT"){
+			cout << "Enter a filename" << endl;
+			cin >> temp1;
+			cout << "Enter a name for the lfs" << endl;
+			cin >> temp2;
+			import(temp1, temp2);
+		}
+		else if(response == "REMOVE"){
+			cout << "Enter the name of the lfs to remove" << endl;
+			cin >> temp1;
+			remove(temp1);
+		}
+		else if(response == "DISPLAY"){
+			cout << "Enter the name of the lfs to display" << endl;
+			cin >> temp1;
+			cout << "Enter the amount to display" << endl;
+			cin >> temp2;
+			cout << "Enter the starting byte for the display to begin at" << endl;
+			cin>> temp3;
+			display(temp1, temp2, temp3);
+		}
+		else if(response == "LIST"){
+			list();
+		}
+		else if(response == "CLEAN"){
+			cout << "Enter the number of segments you wish to clean" << endl;
+			cin >> temp1;
+			clean(temp1);
+		}
+		else if(response == "EXIT"){
+			exit();
+			state = false;
+		}
+		else{
+			cout << "Invalid Response" << endl;
+		}
+	}
 	return 0;
 }
 
